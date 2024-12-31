1b1258a1cb7a4121fea8df0d3341b0bb0a61a40a
hnowakowski <h.nowakowski.praca@gmail.com>
2024-12-31 15:47:25 +0100

    changed id in order class from string to a heap allocated char array
    this is pointless but i want a better grade for a custom destructor

--- Classes/Order.h
+++ Classes/Order.h
@@ -1,24 +1,23 @@
 #ifndef OOP_PROJECT_ORDER_H
 #define OOP_PROJECT_ORDER_H
 
-#include <string>
-
 #include "Cart.h"
 #include "Customer.h"
 
 class Order
 {
-    std::string               id;
+    char*                     id;
     std::shared_ptr<Customer> customer;
     std::shared_ptr<Cart>     cart;
 
   public:
-    std::string               GetId();
+    char*                     GetId();
     std::shared_ptr<Customer> GetCustomer();
     std::shared_ptr<Cart>     GetCart();
 
     Order& operator=(const Order& other);
-    Order(std::string id, std::shared_ptr<Customer> customer, std::shared_ptr<Cart> cart);
+    Order(char id[], std::shared_ptr<Customer> customer, std::shared_ptr<Cart> cart);
+    ~Order();
 };
 
 #endif // OOP_PROJECT_ORDER_H
