71efd2d3727efa85a14e5b58708817c3e5dfa070
hnowakowski <h.nowakowski.praca@gmail.com>
2025-04-17 22:10:29 +0200

    Add libsodium and edit README

--- project/main.cpp
+++ project/main.cpp
@@ -1,8 +1,13 @@
 #include <QApplication>
 #include "mainwindow.h"
+#include <sodium.h>
 
 int main(int argc, char *argv[])
 {
+    if (sodium_init() < 0) {
+        qDebug() << "Initialising libsodium failed, exiting";
+        return 1;
+    }
     QApplication a(argc, argv);
     a.setWindowIcon(QIcon(":/icons/logo.ico"));
     MainWindow w;
